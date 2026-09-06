0f41938d6c1c51e50329e613ee1078c70aa11670
Devansh Sahu <devanshsahu1006@gmail.com>
2026-09-06 11:14:21 +0530

    Update function signatures to use new types

--- include/embstrin_api.h
+++ include/embstrin_api.h
@@ -7,15 +7,15 @@
 
 // ---- Program management ----
 EMBSTRIN_Program* EMBSTRIN_INFRA_API_create_program(uint8_t* instructions,
-                                                     uint32_t buf_size,
-                                                     uint32_t instruction_count,
-                                                     uint32_t SRAM_req,
-                                                     uint32_t flash_req);
+                                                     instruction_count_t buf_size,
+                                                     instruction_count_t instruction_count,
+                                                     mem_size_t SRAM_req,
+                                                     mem_size_t flash_req);
 
 void EMBSTRIN_INFRA_API_destroy_program(EMBSTRIN_Program* program);
 
 // ---- Queue management ----
-EMBSTRIN_ProgQueue* EMBSTRIN_INFRA_API_create_queue(uint32_t capacity);
+EMBSTRIN_ProgQueue* EMBSTRIN_INFRA_API_create_queue(queue_size_t capacity);
 
 void EMBSTRIN_INFRA_API_destroy_queue(EMBSTRIN_ProgQueue* queue);
 
@@ -31,28 +31,28 @@ uint32_t EMBSTRIN_INFRA_API_queue_count(EMBSTRIN_ProgQueue* queue);
 // ---- Task management ----
 EMBSTRIN_Task* EMBSTRIN_INFRA_API_create_task(EMBSTRIN_Program* program,
                                                uint32_t priority,
-                                               uint32_t parent_id,
-                                               uint32_t ttl);
+                                               task_id_t parent_id,
+                                               ttl_t ttl);
 
 EMBSTRIN_Task* EMBSTRIN_INFRA_API_get_task(EMBSTRIN_Device* device,
-                                            uint32_t task_id);
+                                            task_id_t task_id);
 
 int32_t EMBSTRIN_INFRA_API_update_task_state(EMBSTRIN_Task* task,
-                                              int32_t new_state);
+                                              task_state_t new_state);
 
 void EMBSTRIN_INFRA_API_destroy_task(EMBSTRIN_Task* task);
 
 // ---- Device management ----
 EMBSTRIN_Device* EMBSTRIN_INFRA_API_register_device(EMBSTRIN_Host* host,
-                                                      uint32_t device_id,
-                                                      uint32_t device_type,
-                                                      uint32_t task_queue_max);
+                                                      device_id_t device_id,
+                                                      device_type_t device_type,
+                                                      queue_size_t taks_queue_max);
 
 int32_t EMBSTRIN_INFRA_API_remove_device(EMBSTRIN_Host* host,
-                                          uint32_t device_id);
+                                          device_id_t device_id);
 
 EMBSTRIN_Device* EMBSTRIN_INFRA_API_get_device(EMBSTRIN_Host* host,
-                                                uint32_t device_id);
+                                                device_id_t device_id);
 
 int32_t EMBSTRIN_INFRA_API_assign_task(EMBSTRIN_Device* device,
                                         EMBSTRIN_Task* task);
@@ -64,13 +64,12 @@ int32_t EMBSTRIN_INFRA_API_add_neighbour(EMBSTRIN_Device* device,
                                           EMBSTRIN_Device* neighbour);
 
 int32_t EMBSTRIN_INFRA_API_remove_neighbour(EMBSTRIN_Device* device,
-                                             uint32_t neighbour_id);
+                                             device_id_t neighbour_id);
 
 // ---- Host management ----
-EMBSTRIN_Host* EMBSTRIN_INFRA_API_create_host(uint32_t device_capacity,
-                                               uint32_t queue_capacity);
+EMBSTRIN_Host* EMBSTRIN_INFRA_API_create_host(queue_size_t device_capacity,
+                                               queue_size_t queue_capacity);
 
 void EMBSTRIN_INFRA_API_destroy_host(EMBSTRIN_Host* host);
-
 
 #endif
